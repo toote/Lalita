5ea80479a499809ed3b775676a7890f86f4da286
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-11-30 20:04:07 -0300

    Empernator v1

--- lalita/plugins/empernator.py
+++ lalita/plugins/empernator.py
@@ -3,16 +3,36 @@
 # License: GPL v3
 # For further info, see LICENSE file
 
-import random
+from random import choice
 
 from lalita import Plugin
 
 class Empernator(Plugin):
 
 	def init(self, config):
+		self.users = dict()
 		self.register(self.events.COMMAND, self.empern, ['empern'])
+		self.register(self.events.PUBLIC_MESSAGE, self.add_user)
+		self.register(self.events.JOIN, self.add_user)
+		self.register(self.events.LEFT, self.rm_user)
+		self.register(self.events.ACTION, self.add_user)
 
 	def empern(self, user, channel, *args):
-		auser = 'aseba'
-		self.say(channel, u"I empern %s", auser)
+		if not self.users or not self.users[channel]:
+			self.say(channel, u"I have no people to empern")
+		else:
+			auser = choice(self.users[channel])
+			self.say(channel, u"I empern %s", auser)
+	
+	def rm_user(self, user, channel, *args):
+		if channel in self.users:
+			if user in self.users[channel]:
+				self.users[channel].pop(self.users[channel].index(user))
 
+	def add_user(self, user, channel, *args):
+		if channel in self.users:
+			if not user in self.users[channel]:
+				self.users[channel].append(user)
+		else:
+			self.users[channel] = list()
+			self.users[channel].append(user)

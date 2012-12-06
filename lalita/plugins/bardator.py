909b61390d58cd7ff2171f943a80ba070ca78b14
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-12-06 16:25:04 -0300

    Added bardator

---
+++ lalita/plugins/bardator.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012 Sebastian Alvarez <sebastianmalvarez@gmail.com>
+# License: GPL v3
+# For further info, see LICENSE file
+
+from random import choice
+
+from lalita import Plugin
+
+class Bardator(Plugin):
+
+	def init(self, config):
+		self.users = dict()
+		self.register(self.events.COMMAND, self.bard, ['bard'])
+		self.bards = [u"Muchachooo hay que tener cuidadín"]
+
+	def bard(self, user, channel, command, *args):
+		for user in args:
+			bard = choice(self.bards)
+			self.say(channel, u"%s %s", user, bard)
